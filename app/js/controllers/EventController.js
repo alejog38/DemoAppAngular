038f63015f029d24fcab9f67b9ca7d8b66c4fa11
Gary Wilson <wilson,g1987@gmail.com>
2018-03-15 23:41:20 +0000

    Controllers, Handling Events, ng-repeat, Directives

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -11,7 +11,42 @@ eventsApp.controller('EventController',
                     city: "Mountain View",
                     province: 'CA'
                 },
-                imgUrl: 'img/angularjs-logo.png'
+                imgUrl: 'img/angularjs-logo.png',
+                sessions: [
+                    {
+                        name: 'Directives Masterclass',
+                        creatorName: 'Bob Smith',
+                        duration: '1 hr',
+                        level: 'Advanced',
+                        abstract: 'In this session you will learn the ins and outs of directives!',
+                        upVoteCount: 0
+
+                    },
+                    {
+                        name: 'Scopes for fun and profit',
+                        creatorName: 'John Doe',
+                        duration: '30 mins',
+                        level: 'Introductory',
+                        abstract: 'This session will take a closer look at scopes.',
+                        upVoteCount: 0
+                    },
+                    {
+                        name: 'Well Behaved Controllers',
+                        creatorName: 'John Doe',
+                        duration: '2 hours',
+                        level: 'Intermediate',
+                        abstract: 'Controllers are the beginning of everything, Learn how to craft controllers that will win the respect of your friends and neighbours.',
+                        upVoteCount: 0
+                    }
+                ]
             }
+
+            $scope.upVoteSession = function(session){
+                session.upVoteCount++;
+            };
+
+            $scope.downVoteSession = function(session){
+                session.upVoteCount--;
+            };
         }
     );
