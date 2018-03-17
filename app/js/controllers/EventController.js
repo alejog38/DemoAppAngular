a4a2a6c337ab1eccc56fa4ee60f0533a478dccce
Gary Wilson <wilson,g1987@gmail.com>
2018-03-17 20:45:29 +0000

    Controllers and Markup

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -2,7 +2,8 @@
 
 eventsApp.controller('EventController',
         function($scope){
-            $scope.event = {
+        $scope.sortorder = 'name';
+        $scope.event = {
                 name: 'Angular Boot Camp',
                 date: '1/1/2013',
                 time: '10:30 am',
@@ -16,7 +17,7 @@ eventsApp.controller('EventController',
                     {
                         name: 'Directives Masterclass',
                         creatorName: 'Bob Smith',
-                        duration: '1 hr',
+                        duration: 1,
                         level: 'Advanced',
                         abstract: 'In this session you will learn the ins and outs of directives!',
                         upVoteCount: 0
@@ -25,7 +26,7 @@ eventsApp.controller('EventController',
                     {
                         name: 'Scopes for fun and profit',
                         creatorName: 'John Doe',
-                        duration: '30 mins',
+                        duration: 2,
                         level: 'Introductory',
                         abstract: 'This session will take a closer look at scopes.',
                         upVoteCount: 0
@@ -33,13 +34,13 @@ eventsApp.controller('EventController',
                     {
                         name: 'Well Behaved Controllers',
                         creatorName: 'John Doe',
-                        duration: '2 hours',
+                        duration: 4,
                         level: 'Intermediate',
                         abstract: 'Controllers are the beginning of everything, Learn how to craft controllers that will win the respect of your friends and neighbours.',
                         upVoteCount: 0
                     }
                 ]
-            }
+            };
 
             $scope.upVoteSession = function(session){
                 session.upVoteCount++;
