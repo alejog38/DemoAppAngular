d3139b860f277dd75b48976e2dfe9982634af576
Gary Wilson <wilson,g1987@gmail.com>
2018-03-18 19:15:47 +0000

    Service Added to pull event details from local JSON file, IIS method implemented

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -1,53 +1,19 @@
 'use strict';
 
 eventsApp.controller('EventController',
-        function($scope){
+        function EventController($scope, eventData, $log){
         $scope.sortorder = 'name';
-        $scope.event = {
-                name: 'Angular Boot Camp',
-                date: '1/1/2013',
-                time: '10:30 am',
-                location : {
-                    address: "Google Headquarters",
-                    city: "Mountain View",
-                    province: 'CA'
-                },
-                imgUrl: 'img/angularjs-logo.png',
-                sessions: [
-                    {
-                        name: 'Directives Masterclass',
-                        creatorName: 'Bob Smith',
-                        duration: 1,
-                        level: 'Advanced',
-                        abstract: 'In this session you will learn the ins and outs of directives!',
-                        upVoteCount: 0
+        eventData.getEvent()
+            .success(function (event) { $scope.event = event; })
+            .error(function(data, status, headers, config){
+                $log.warn(data, status, headers(), config);
+            });
 
-                    },
-                    {
-                        name: 'Scopes for fun and profit',
-                        creatorName: 'John Doe',
-                        duration: 2,
-                        level: 'Introductory',
-                        abstract: 'This session will take a closer look at scopes.',
-                        upVoteCount: 0
-                    },
-                    {
-                        name: 'Well Behaved Controllers',
-                        creatorName: 'John Doe',
-                        duration: 4,
-                        level: 'Intermediate',
-                        abstract: 'Controllers are the beginning of everything, Learn how to craft controllers that will win the respect of your friends and neighbours.',
-                        upVoteCount: 0
-                    }
-                ]
-            };
+        $scope.upVoteSession = function(session){
+            session.upVoteCount++;
+        };
 
-            $scope.upVoteSession = function(session){
-                session.upVoteCount++;
-            };
-
-            $scope.downVoteSession = function(session){
-                session.upVoteCount--;
-            };
+        $scope.downVoteSession = function(session){
+            session.upVoteCount--;
         }
-    );
+    });
--- scripts/web-server.js
+++ scripts/web-server.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var path = require('path');
+var events = require('./eventsController');
+
 var app = express();
 var rootPath = path.normalize(__dirname + '/../');
 
 app.use(express.static(rootPath + '/app'));
+
+app.get('data/event/:id', events.get);
+app.post('data/event/:id', events.save);
 
 var port = 8080;
 app.listen(port);
